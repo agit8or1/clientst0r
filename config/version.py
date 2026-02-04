0233cbc37d04aee0d318970f80304b3628d2b4e6
Luna the GSD <agit8or@agit8or.net>
2026-02-04 17:07:54 +0000

    Update version to v2.49.4

--- config/version.py
+++ config/version.py
@@ -2,11 +2,11 @@
 Version information for HuduGlue
 """
 
-VERSION = '2.49.3'
+VERSION = '2.49.4'
 VERSION_INFO = {
     'major': 2,
     'minor': 49,
-    'patch': 3,
+    'patch': 4,
     'status': 'stable',  # alpha, beta, rc, stable
 }
 
