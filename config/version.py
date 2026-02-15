ab3241ce223dab2d31939d726ea1e94c399bbb8d
Luna the GSD <agit8or@agit8or.net>
2026-02-15 20:37:55 +0000

    Test v2.91.1

--- config/version.py
+++ config/version.py
@@ -2,11 +2,11 @@
 Version information for Client St0r
 """
 
-VERSION = '2.91.0'
+VERSION = '2.91.1'
 VERSION_INFO = {
     'major': 2,
     'minor': 91,
-    'patch': 0,
+    'patch': 1,
     'status': 'stable',  # alpha, beta, rc, stable
 }
 
