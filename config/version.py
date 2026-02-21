6881f654fa7c162b889614dc9d335b274dc1c4c5
Luna the GSD <agit8or@agit8or.net>
2026-02-21 04:35:56 +0000

    Bump version to 3.6.2

--- config/version.py
+++ config/version.py
@@ -2,11 +2,11 @@
 Version information for Client St0r
 """
 
-VERSION = '3.6.1'
+VERSION = '3.6.2'
 VERSION_INFO = {
     'major': 3,
     'minor': 6,
-    'patch': 1,
+    'patch': 2,
     'status': 'stable',  # alpha, beta, rc, stable
 }
 
